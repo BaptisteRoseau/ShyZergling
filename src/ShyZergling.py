8b0891d0d5ee70613cae00f5c7cfded27253808e
Baptiste ROSEAU <broseau@gmail.com>
2019-05-26 15:55:49 +0200

    Update ShyZergling.py

--- src/ShyZergling.py
+++ src/ShyZergling.py
@@ -5,9 +5,9 @@ from asyncio import PriorityQueue
 
 import sc2
 from sc2 import Race, Difficulty
-from sc2.constants import *
 from sc2.player import Bot, Computer
 from sc2.data import race_townhalls
+from sc2.constants import LARVA, DRONE, OVERLORD
 
 import enum
 
@@ -20,8 +20,8 @@ class ShyZergling(sc2.BotAI):
 
     async def on_step(self, iteration):
         self.larvae = self.units(LARVA)
-        self.distribute_worker()
-        self.build_from_queues(self)
+        self.distribute_workers()
+        self.build_from_queues()
 
     #### SUBFUNCTIONS
 
@@ -31,17 +31,17 @@ class ShyZergling(sc2.BotAI):
         await self.build_units()
         
     async def build_buildings(self):
-        while not building_queue.empty():
-            building = building_queue.get()
+        while not self.building_queue.empty():
+            building = self.building_queue.get()
             drones = self.units(DRONE)
             if self.can_afford(building) and drones.exists:
-                await self.do(drones.random.build(building, near=self.race_townhalls.first))
+                await self.do(drones.random.build(building, near=self.townhalls.first))
 
     async def build_units(self):
-        while not unit_queue.empty():
-            unit = unit_queue.get()
+        while not self.unit_queue.empty():
+            unit = self.unit_queue.get()
             if self.can_afford(unit) and self.larvae.exists:
-                await self.do(larvae.random.train(unit))
+                await self.do(self.larvae.random.train(unit))
 
     async def extend_supply(self):
         if self.supply_left < 2:
@@ -52,7 +52,7 @@ class ShyZergling(sc2.BotAI):
 def main():
     sc2.run_game(sc2.maps.get("CatalystLE"), [
         Bot(Race.Zerg, ShyZergling()),
-        Computer(Race.Terran, Difficulty.Medium)
+        Computer(Race.Terran, Difficulty.Easy)
     ], realtime=False, save_replay_as="ZvT.SC2Replay")
 
 if __name__ == '__main__':
